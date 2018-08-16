fd85dec1141010769ade0ee6dbef1344e8c85b87
mlavi16 <maya.lavi11@gmail.com>
2018-08-16 11:00:46 -0400

    new

--- finalProject.js
+++ finalProject.js
@@ -6,7 +6,6 @@ window.onload = function(){
     var span = document.getElementsByClassName("close")[0];
     btn.onclick = function() {
         modal.style.display = "block";
-        console.log("hi");
     }
     span.onclick = function() {
         modal.style.display = "none";
@@ -22,7 +21,6 @@ window.onload = function(){
     var span2 = document.getElementsByClassName("close")[1];
     btn2.onclick = function() {
         modal2.style.display = "block";
-        console.log("hello");
     }
     span2.onclick = function() {
         modal2.style.display = "none";
@@ -32,6 +30,50 @@ window.onload = function(){
             modal2.style.display = "none";
         }
     }
-
+    //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
+    var modal3 = document.getElementById("myModal3");
+    var btn3 = document.getElementById("myBtn3");
+    var span3 = document.getElementsByClassName("close")[2];
+    btn3.onclick = function() {
+        modal3.style.display = "block";
+    }
+    span3.onclick = function() {
+        modal3.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal3) {
+            modal3.style.display = "none";
+        }
+    }
+    //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
+    var modal4 = document.getElementById("myModal4");
+    var btn4 = document.getElementById("myBtn4");
+    var span4 = document.getElementsByClassName("close")[3];
+    btn4.onclick = function() {
+        modal4.style.display = "block";
+    }
+    span4.onclick = function() {
+        modal4.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal4) {
+            modal4.style.display = "none";
+        }
+    }
+    //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
+    var modal5 = document.getElementById("myModal5");
+    var btn5 = document.getElementById("myBtn5");
+    var span5 = document.getElementsByClassName("close")[4];
+    btn5.onclick = function() {
+        modal5.style.display = "block";
+    }
+    span5.onclick = function() {
+        modal5.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal5) {
+            modal5.style.display = "none";
+        }
+    }
 
 };
