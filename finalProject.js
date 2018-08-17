f1e1525e22b8828819afc07f39c631b3d69d8b1c
mlavi16 <maya.lavi11@gmail.com>
2018-08-17 13:55:17 -0400

    Added updates to all files

--- finalProject.js
+++ finalProject.js
@@ -1,52 +1,90 @@
-
+// This is for the Guess section
 window.onload = function(){
     
     var modal = document.getElementById("myModal");
     var btn = document.getElementById("myBtn");
     var span = document.getElementsByClassName("close")[0];
     btn.onclick = function() {
-        modal.style.display = "block";
+        console.log("print");
+        // modal.style.display = "block";
     }
     span.onclick = function() {
         modal.style.display = "none";
     }
-    //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
+    jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
     var modal2 = document.getElementById("myModal2");
     var btn2 = document.getElementById("myBtn2");
-    var span2 = document.getElementsByClassName("close")[1];
+     var span2 = document.getElementsByClassName("close")[1];
     btn2.onclick = function() {
         modal2.style.display = "block";
-    }
-    span2.onclick = function() {
+     }
+     span2.onclick = function() {
         modal2.style.display = "none";
-    }
-    //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
-    var modal3 = document.getElementById("myModal3");
-    var btn3 = document.getElementById("myBtn3");
-    var span3 = document.getElementsByClassName("close")[2];
-    btn3.onclick = function() {
-        modal3.style.display = "block";
-    }
-    span3.onclick = function() {
-        modal3.style.display = "none";
-    }
-    //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
-    var modal4 = document.getElementById("myModal4");
-    var btn4 = document.getElementById("myBtn4");
+     }
+     //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
+     var modal3 = document.getElementById("myModal3");
+     var btn3 = document.getElementById("myBtn3");
+     var span3 = document.getElementsByClassName("close")[2];
+     btn3.onclick = function() {
+         modal3.style.display = "block";
+     }
+     span3.onclick = function() {
+         modal3.style.display = "none";
+     }
+     //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
+     var modal4 = document.getElementById("myModal4");
+     var btn4 = document.getElementById("myBtn4");
     var span4 = document.getElementsByClassName("close")[3];
-    btn4.onclick = function() {
-        modal4.style.display = "block";
-    }
-    span4.onclick = function() {
-        modal4.style.display = "none";
-    }
-    //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
-    var modal5 = document.getElementById("myModal5");
-    var btn5 = document.getElementById("myBtn5");
-    var span5 = document.getElementsByClassName("close")[4];
-    btn5.onclick = function() {
-        modal5.style.display = "block";
-    }
+     btn4.onclick = function() {
+         modal4.style.display = "block";
+     }
+     span4.onclick = function() {
+         modal4.style.display = "none";
+     }
+     //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
+     var modal5 = document.getElementById("myModal5");
+     var btn5 = document.getElementById("myBtn5");
+     var span5 = document.getElementsByClassName("close")[4];
+     btn5.onclick = function() {
+         modal5.style.display = "block";
+     }
     span5.onclick = function() {
         modal5.style.display = "none";
+     }
+
+
+// This is for Urban Outfitters
+ window.onload = function(){
+    
+    var modal = document.getElementById("myModal");
+        var btn = document.getElementById("myBtn");
+        var span = document.getElementsByClassName("close")[0];
+        btn.onclick = function() {
+            console.log("THISDKSNFLDSNF");
+            modal.style.display = "block";
+        }
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+        //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
+        var modal2 = document.getElementById("myModal2");
+        var btn2 = document.getElementById("myBtn2");
+        var span2 = document.getElementsByClassName("close")[1];
+        btn2.onclick = function() {
+            modal2.style.display = "block";
+        }
+        span2.onclick = function() {
+            modal2.style.display = "none";
+        }
+        //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
+        var modal3 = document.getElementById("myModal3");
+        var btn3 = document.getElementById("myBtn3");
+        var span3 = document.getElementsByClassName("close")[2];
+        btn3.onclick = function() {
+            modal3.style.display = "block";
+        }
+        span3.onclick = function() {
+            modal3.style.display = "none";
+        }
+        //jEAHFAK,JSNFDKAMDZNXmansD KJWADBFJWAMSD,JlndsiuKJEARKJWADSNMKJASS
     }
--- modal-images.js
+++ modal-images.js
@@ -9,7 +9,8 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
-    modal.style.display = "block";
+    console.log("print");
+    // modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
